d2d87d9064ed470b47dbded23bd8fba380dd0059
Mathivanan Kathirvel <mathimcafee@gmail.com>
2026-05-06 18:29:16 +0000

    force: rename bundle to app-*.js to bust all CDN caches

--- vite.config.js
+++ vite.config.js
@@ -16,8 +16,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: `assets/[name]-[hash]-v2.js`,
-        chunkFileNames: `assets/[name]-[hash]-v2.js`,
+        entryFileNames: `assets/app-[hash].js`,
+        chunkFileNames: `assets/chunk-[hash].js`,
       }
     }
   }
