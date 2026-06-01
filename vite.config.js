a4cfbc0d98797285fcbe1047c3aadb03a9a1ef74
Mathivanan Kathirvel <mathimcafee@gmail.com>
2026-06-01 16:29:18 +0530

    chore: trigger build 1780311558628

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-// BUILD: 1780300599493
+// BUILD: 1780311558628
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
